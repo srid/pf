a26feb2176eae82760a534909ae8b5c21429dde7
Sridhar Ratnakumar <sridhar.ratna@gmail.com>
2010-04-09 12:54:19 -0700

    initial layout

---
+++ README.rst
@@ -0,0 +1,17 @@
+pf
+==
+
+Since I got frustrated typing "less src/foo/bar/baz/afile.py" all the time, 
+I wrote this tool. How to use it? You can find it yourself by just playing 
+with it.
+
+Credits
+-------
+
+- `Distribute`_
+- `Buildout`_
+- `modern-package-template`_
+
+.. _Buildout: http://www.buildout.org/
+.. _Distribute: http://pypi.python.org/pypi/distribute
+.. _`modern-package-template`: http://pypi.python.org/pypi/modern-package-template
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.5'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='pf',
+    version=version,
+    description="Programmer's find; smart file-finding; no more repeated double-tabs.",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='',
+    author='Sridhar Ratnakumar',
+    author_email='sridhar.ratna@gmail.com',
+    url='http://github.com/srid/pf',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['pf=pf:main']
+    }
+)
